acfa28f84f4cb3bcd77482df61eecbf3f4276c88
vivek <ikumarvivek786@gmail.com>
2022-10-13 20:30:53 +0530

    UC-1_FirstName

---
+++ RegexAssignment/Patterns.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexAssignment
+{
+    public class Patterns
+    {
+        public static string FirstName = "^[A-Z][a-z]{3,}";
+        public void Validation(string sample)
+        {
+            if (Regex.IsMatch(sample, FirstName))
+                Console.WriteLine("Valid FirstName");
+            else
+                Console.WriteLine("Invalid FirstName");
+        }
+    }
+}
---
+++ RegexAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexAssignment
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registratuiuon Program");
+
+            Patterns patterns = new Patterns();
+            Console.WriteLine("Enter the First name");
+            string FirstName = Console.ReadLine();
+            patterns.Validation(FirstName);
+            Console.ReadLine();
+        }
+    }
+}
